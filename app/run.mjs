0a313fbaed14327ef3be8156f418d7b16d120579
Francois Seguin <seguin.mtl@gmail.com>
2023-09-09 16:54:22 -0400

    adding find command

--- app/run.mjs
+++ app/run.mjs
@@ -17,7 +17,8 @@ const run = program.name("run");
 
 run.command("devices").description("list of all devices on network");
 run.command("sampling").description("export youtube to wav");
-run.command("repo").description("convert a github repo to a text file");
+run.command("repo").description("convert a github repo to a text");
+run.command("find").description("find a tag in a notion database");
 
 try {
   program.parse(["--help"], { from: "user" });
